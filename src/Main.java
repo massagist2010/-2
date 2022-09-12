57707086f3abc1ffb178025428ba25ad788fc219
massagist2010 <rusy_boob2@mail.ru>
2022-09-12 22:29:48 +0300

    переменная ч.2. зд.4

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
 public class Main {
     public static void main(String[] args) {;
-        //третья задача
-        byte lp = 23;
-        byte as =27;
-        byte ea = 30;
-        short paper = 480;
-        int students = lp + as + ea;
-        int paperOnOne = paper / students;
-        System.out.println("Для каждого ученика расчитано " + paperOnOne + " листов бумаги");
+        //четвертая задача
+        byte bottle = 16;
+        byte min = 2;
+        int bIn1Min = bottle / min;
+        System.out.println("бутылок в минуту " + bIn1Min );
+        byte ch = 60;
+        byte day = 24;
+        int aloneInDay = day * 60;
+        System.out.println(aloneInDay + " min. in day");
+
+        byte pOn20M =  20;
+        int b20M = bottle / min * pOn20M;
+        System.out.println("за 20 минут машина произвела бутылок " + b20M + " штук");
+
+        int bottlesInDay = aloneInDay * bottle;
+        System.out.println("За 1 день машина произвела бутылок " + bottlesInDay + " штук");
+
+        byte week3 = 3;
+        int aloneInWeek = week3 * aloneInDay * bIn1Min;
+        System.out.println("За 3 дня машина произвела бутылок " + aloneInWeek + " штук");
+
+        byte m = 30;
+        long aloneInMonth = m * aloneInDay * bIn1Min;
+        System.out.println("За 1 месяц машина произвела бутылок " + aloneInMonth + " штук");
+
 
     }
     }
