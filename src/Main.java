3946996511b4b8fd1512e240763986988cfb4c9b
massagist2010 <rusy_boob2@mail.ru>
2022-09-13 00:20:19 +0300

    переменная ч.2. зд.6

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,25 @@
 public class Main {
     public static void main(String[] args) {;
-        //пятая задача:
-      byte dye = 120;
-      byte white = 2;
-      byte brown = 4;
-      int InKlass = white + brown;
-      byte aloneKlass = 120 / 6;
-      int aloneWhite = aloneKlass * white;
-      int aloneBrown = aloneKlass * brown;
-        System.out.println(" В школе, где " + aloneKlass + " классов, нужно " + aloneWhite + " банок белой краки и " + aloneBrown + " банок коричневой краски");
+        //шестая задача:
+      byte banana = 5;
+      byte oneBanana = 80;
+      int weightBananas = banana * oneBanana;
+      int milk = 200;
+      byte milk1 = 100;
+      byte milk2 = 105;
+      long weightMilk = milk / milk1 * milk2;
+      byte ice = 2;
+      byte oneIce = 100;
+      int twoIce = ice * oneIce;
+      byte eggs = 4;
+      byte oneEgg = 70;
+      int eggs4 = eggs * oneEgg;
 
+      long aloneGrams = eggs4 + twoIce + weightMilk + weightBananas;
+        System.out.println(aloneGrams);
+      short kg = 1000;
+
+      double theWeightKg = aloneGrams / kg;
+        System.out.println("вес в кг. будет " + theWeightKg);
     }
     }
