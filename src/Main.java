3bbfbbbd1fc72885b939937d5ad0ed4ea751b03c
massagist2010 <rusy_boob2@mail.ru>
2022-09-12 23:04:33 +0300

    переменная ч.2. зд.5

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,14 @@
 public class Main {
     public static void main(String[] args) {;
-        //четвертая задача
-        byte bottle = 16;
-        byte min = 2;
-        int bIn1Min = bottle / min;
-        System.out.println("бутылок в минуту " + bIn1Min );
-        byte ch = 60;
-        byte day = 24;
-        int aloneInDay = day * 60;
-        System.out.println(aloneInDay + " min. in day");
-
-        byte pOn20M =  20;
-        int b20M = bottle / min * pOn20M;
-        System.out.println("за 20 минут машина произвела бутылок " + b20M + " штук");
-
-        int bottlesInDay = aloneInDay * bottle;
-        System.out.println("За 1 день машина произвела бутылок " + bottlesInDay + " штук");
-
-        byte week3 = 3;
-        int aloneInWeek = week3 * aloneInDay * bIn1Min;
-        System.out.println("За 3 дня машина произвела бутылок " + aloneInWeek + " штук");
-
-        byte m = 30;
-        long aloneInMonth = m * aloneInDay * bIn1Min;
-        System.out.println("За 1 месяц машина произвела бутылок " + aloneInMonth + " штук");
-
+        //пятая задача:
+      byte dye = 120;
+      byte white = 2;
+      byte brown = 4;
+      int InKlass = white + brown;
+      byte aloneKlass = 120 / 6;
+      int aloneWhite = aloneKlass * white;
+      int aloneBrown = aloneKlass * brown;
+        System.out.println(" В школе, где " + aloneKlass + " классов, нужно " + aloneWhite + " банок белой краки и " + aloneBrown + " банок коричневой краски");
 
     }
     }
