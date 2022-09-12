4cbeabefe3d5b9617e52ba21d2624b9c6931b7cd
massagist2010 <rusy_boob2@mail.ru>
2022-09-12 17:38:51 +0300

    переменная ч.2. зд.1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {;
+        byte one = 1;
+        System.out.println("значение переменной " + one + " с типом byte равно 1 " );
+
+        short two = 2;
+        System.out.println("значение переменной " + two + " с типом short равно 2 " );
+        int three = 3;
+        System.out.println("значение переменной " + three + " с типом int равно 3 " );
+        long four = 4;
+        System.out.println("значение переменной " + four + " с типом long равно " + four);
+        float no = 1.0f;
+        System.out.println("значение переменной " + no + " с типом float равно  " + no );
+
+        double dontNo = 2.8;
+        System.out.println("значение переменной " + dontNo + " с типом double равно " + dontNo);
+    }
+}
