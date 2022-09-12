faaff665176dbdd794a819fe6697ac5662fbdbf0
massagist2010 <rusy_boob2@mail.ru>
2022-09-12 19:46:32 +0300

    переменная ч.2. зд.3

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,13 @@
 public class Main {
     public static void main(String[] args) {;
-        //вторая задача
-        byte by = 2;
-        System.out.println(by);
-        int in = 27897;
-        System.out.println(in);
-        long lo = 987678965549L;
-        System.out.println(lo);
-        short sho = -159;
-        System.out.println(sho);
-        var v = 569;
-        System.out.println(v);
+        //третья задача
+        byte lp = 23;
+        byte as =27;
+        byte ea = 30;
+        short paper = 480;
+        int students = lp + as + ea;
+        int paperOnOne = paper / students;
+        System.out.println("Для каждого ученика расчитано " + paperOnOne + " листов бумаги");
 
-        float fl = 27.12f;
-        System.out.println(fl);
-        double dob = 786;
-        System.out.println(dob);
-
-        char ch = 67;
-        System.out.println(ch);
-        boolean boo = false;
-        System.out.println(boo);
     }
     }
