95553bc37678d348fc589078169bd4905b213532
massagist2010 <rusy_boob2@mail.ru>
2022-09-13 11:45:02 +0300

    переменная ч.2. зд.8

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,32 @@
 public class Main {
     public static void main(String[] args) {;
-        //седьмая задача:
-        byte lose = 7;
-        short weight = 1000;
-        int loseWeight = lose * weight;
+        //восьмая задача:
+        int masha = 67760;
 
-        short gr = 250;
-        int totalDay250 = loseWeight / gr;
-        System.out.println("всего дней, если спортсмен теряет 250гр.в день, " +totalDay250);
+        int dan = 83690;
+        int kris = 76230;
+        byte inYear = 12;
+        double promotion = 0.1;
 
-        short gr2 = 500;
-        int totalDay500 = loseWeight / gr2;
-        System.out.println("дней, если спортсмен теряет 500гр. в день, " +totalDay500);
+        double manyD = dan * promotion;
+        double manyDan = dan + manyD;
+        int nowD = dan * inYear;
+        double newD = manyDan * inYear;
+        double differ = newD - nowD;
+        System.out.println("Денис теперь получает " + newD + " .годовой доход увеличен на " + differ);
 
+        double maniK = kris * promotion;
+        double manyKris = kris + maniK;
+        int nowK = kris * inYear;
+        double newK = manyKris * inYear;
+        double diffK = newK - nowK;
+        System.out.println("Кристина теперь получает " + newK + " .годовой доход увеличен на " + diffK);
 
-        byte day01 = 28;
-        byte day02 = 14;
-        int daysWeight = day02 + day01;
-        int daysOnAverage = daysWeight / lose;
-        System.out.println("дней в среднем на похудение " +daysOnAverage);
+        double maniM = masha * promotion;
+        double manyMasha = masha + maniM;
+        int nowM = masha * inYear;
+        double newM = manyMasha * inYear;
+        double difM = newM - nowM;
+        System.out.println(" Маша теперь получает " + newM + " .годовой доход увеличен на " + difM);
     }
     }
